d0524158bb722c11868f171e7c744fcfbb7b060b
Fernando Juarez <juarezsfw@outlook.com>
2024-01-05 16:11:21 -0600

    arreglos 2D 5-ene-2023

--- Arreglos/src/Main.java
+++ Arreglos/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        SingleDimensionArray sda = new SingleDimensionArray(10);
+
+        // 1D Array
+        /*
+         *
+         *  SingleDimensionArray sda = new SingleDimensionArray(10);
         sda.insert(0, 0);
         sda.insert(1, 10);
         sda.insert(2, 20);
@@ -16,12 +22,27 @@ public class Main {
 
         System.out.println("Array traversal");
         sda.traverseArray();
-       */
+
 
         sda.searchInArray(10);
 
         sda.deleteValue(0);
         System.out.println(sda.arr[0]);
+        */
 
+        TwoDimensionalArray sda = new TwoDimensionalArray(3,3);
+        sda.insertValueInTheArray(0,0,10);
+        sda.insertValueInTheArray(0,1,20);
+        sda.insertValueInTheArray(1,0,30);
+        sda.insertValueInTheArray(1,1,40);
+        sda.accessCell(0,1);
+
+        // System.out.println(Arrays.deepToString(sda.arr));
+
+        sda.traverse2DArray();
+
+        // sda.searchingValue(50);
+        sda.deleteValueFromArray(0,0);
+        System.out.println(Arrays.deepToString(sda.arr));
     }
 }
---
+++ Arreglos/src/TwoDimensionalArray.java
@@ -0,0 +1,74 @@
+import java.lang.reflect.Array;
+
+public class TwoDimensionalArray {
+    int arr[][] = null;
+
+    // Constructor
+    public TwoDimensionalArray(int numberOfRows, int numberOfColumns) {
+        this.arr = new int[numberOfRows][numberOfColumns];
+        for(int row = 0; row < arr.length; row++) {
+            for(int col = 0; col < arr[0].length; col ++ ) {
+                arr[row][col] = Integer.MIN_VALUE;
+            }
+        }
+    }
+
+    // Inserting value in the Array
+    public void insertValueInTheArray(int row, int col, int value) {
+        try {
+            if(arr[row][col] == Integer.MIN_VALUE) {
+                arr[row][col] = value;
+                System.out.println("The value is succesfully inserted");
+            } else {
+                System.out.println("This cell is already occupied");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid idex for 2D array");
+        }
+    }
+
+    // Accessing cell value from given array
+    public void accessCell(int row, int col) {
+
+        System.out.println("\nAccessing Row" + row + " " + " Col# "+ col);
+        try {
+            System.out.println("Cell value is: " + arr[row][col]);
+        }catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index for 2D array");
+        }
+    }
+
+    // Traverse 2D array
+    public void traverse2DArray() {
+        for(int row = 0; row < arr.length; row++) {
+            for(int col=0; col < arr[0].length; col++){
+                System.out.print(arr[row][col] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Searching single value from 2D array
+    public void searchingValue(int value) {
+        for(int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                if (arr[row][col] == value) {
+                    System.out.println("Value " + value + " is in  row " + row + " and col " + col);
+                    return;
+                }
+
+            }
+        }
+        System.out.println("Value is not found");
+    }
+
+    public void deleteValueFromArray(int row, int col) {
+        try {
+            System.out.println("Successfully deleted " + arr[row][col]);
+            arr[row][col] = Integer.MIN_VALUE;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("This is index is not valid for array");
+        }
+    }
+}

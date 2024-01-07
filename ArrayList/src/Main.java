c5933fa4a41f6bef76536325e940cd0db081156b
Fernando Juarez <juarezsfw@outlook.com>
2024-01-06 19:41:33 -0600

    array list creation 6-ene-2023

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(30,10,20)); // Arrays as List
+        System.out.println(numbers);
+
+    }
+}
